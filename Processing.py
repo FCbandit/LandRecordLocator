4fae4c45f97d6eb1da07eb8a1fd254612e0ac3ff
dhan1997 <dannyhan1997@gmail.com>
2025-09-09 07:51:17 -0700

    Added geenerate links function

--- Processing.py
+++ Processing.py
@@ -5,31 +5,61 @@ import datetime
 arcpy.env.overwriteOutput = True
 
 #Folder Location
-main_folder = r"P:\mppub\MAPSVCS\STAFF_WORKING\DHAN\APRX\Canyon_Fire\FirePerimeter_EvacZones"
+main_folder = r"P:\mppub\MAPSVCS\SPECIAL_PROJECTS\Land_Records_Feature_Locator\downloaded_services"
+gdb_name = "LandRecords.gdb"
+gdb_path = os.path.join(main_folder, gdb_name)
 
-# REST service URLs
-rest_services = {
-    "Fire_Perimeters": "https://services1.arcgis.com/jUJYIo9tSA7EHvfZ/ArcGIS/rest/services/CA_Perimeters_NIFC_FIRIS_public_view/FeatureServer/0",
-    "Evac_Zones": "https://services.arcgis.com/RmCCgQtiZLDCtblq/arcgis/rest/services/Public_Emergency_Map_GENASYS_Alert_Warning_Area/FeatureServer/0"
-}
+def downloadServices():
+    # REST service URLs
+    rest_services = {
+        "Parcel_Map": "https://dpw.gis.lacounty.gov/dpw/rest/services/landrecords/MapServer/1",
+        "Tract_Map": "https://dpw.gis.lacounty.gov/dpw/rest/services/landrecords/MapServer/2",
+        "RecordOfSurvey": "https://dpw.gis.lacounty.gov/dpw/rest/services/landrecords/MapServer/0"
+    }
 
-#Create date/timed folder
-timestamp = datetime.datetime.now().strftime("%Y%d%m_%H%M")
-output_folder = os.path.join(main_folder, timestamp)
-os.makedirs(output_folder, exist_ok=True)
-print(f"Created folder: {output_folder}")
+    #Create FGDB
+    arcpy.CreateFileGDB_management(main_folder, gdb_name)
+    print(f"Created GDB: {gdb_path}")
 
-#Create FGDB
-gdb_name = "FireData.gdb"
-gdb_path = os.path.join(output_folder, gdb_name)
-arcpy.CreateFileGDB_management(output_folder, gdb_name)
-print(f"Created GDB: {gdb_path}")
+    #Download services
+    for fc_name, url in rest_services.items():
+        output_fc = os.path.join(gdb_path, fc_name)
+        print(f"Downloading {fc_name} from {url}...")
+        arcpy.conversion.FeatureClassToFeatureClass(url, gdb_path, fc_name)
+        print(f"Saved: {output_fc}")
 
-#Download services
-for fc_name, url in rest_services.items():
-    output_fc = os.path.join(gdb_path, fc_name)
-    print(f"Downloading {fc_name} from {url}...")
-    arcpy.conversion.FeatureClassToFeatureClass(url, gdb_path, fc_name)
-    print(f"Saved: {output_fc}")
+    print("Done.")
 
-print("Done.")
+def generateLinks():
+    #Delete existing link field within parcel map layer
+    arcpy.management.DeleteField(
+        in_table=os.path.join(gdb_path,"Parcel_Map"),
+        drop_field="LINK",
+        method="DELETE_FIELDS"
+    )
+    #make new field with 100 str
+    arcpy.management.AddField(
+        in_table=os.path.join(gdb_path,"Parcel_Map"),
+        field_name="LINK",
+        field_type="TEXT",
+        field_precision=None,
+        field_scale=None,
+        field_length=100,
+        field_alias="Link",
+        field_is_nullable="NULLABLE",
+        field_is_required="NON_REQUIRED",
+        field_domain=""
+    )
+    #calculate field
+    arcpy.management.CalculateField(
+        in_table=os.path.join(gdb_path,"Parcel_Map"),
+        field="LINK",
+        expression='"https://pw.lacounty.gov/sur/nas/landrecords/parcel/" + !REFERENCE!.split("-")[0] + "/" + !REFERENCE! + ".pdf"',
+        expression_type="PYTHON3",
+        code_block="",
+        field_type="TEXT",
+        enforce_domains="NO_ENFORCE_DOMAINS"
+    )
+
+if __name__ == "__main__":
+    generateLinks()
